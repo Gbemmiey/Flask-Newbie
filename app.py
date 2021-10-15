6069ac35dfd18abf133264d72554fede8d33cbd1
Gbemmiey <gbemfemalabi@gmail.com>
2021-10-15 12:52:24 +0100

    Modified the base template file

--- app.py
+++ app.py
@@ -17,7 +17,17 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     comments = {str(name), "goes", "to", "school"}
-    return render_template('User.html', name=name, comments=comments)
+    return render_template('User.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_service_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
