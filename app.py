95fadfe5eec5c497ac9015644f916629170f9da2
Gbemmiey <gbemfemalabi@gmail.com>
2021-10-16 13:41:56 +0100

    Added placeholders to several routing functions fix a bug

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+current_time = datetime.utcnow()
+
+
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    current_time = datetime.utcnow()
     return render_template('index.html', browser_details=user_agent, access_time=current_time)
     # return response, user_agent, redirect
 
@@ -21,27 +23,27 @@ def index():
 @app.route('/User/<name>')
 def user(name):
     comments = {str(name), "goes", "to", "school"}
-    return render_template('User.html', name=name)
+    return render_template('User.html', name=name, access_time=current_time)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', access_time=current_time), 404
 
 
 @app.errorhandler(500)
 def internal_service_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', access_time=current_time), 500
 
 
 @app.route('/Login')
 def login():
-    return render_template('Login.html')
+    return render_template('Login.html', access_time=current_time)
 
 
 @app.route('/Signup')
 def signup():
-    return render_template('Signup.html')
+    return render_template('Signup.html', access_time=current_time)
 
 
 if __name__ == '__main__':
